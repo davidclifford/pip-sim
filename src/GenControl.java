24f62e91d9354dbf8d0eeeea8acfd6318ba152af
David Clifford <david.clifford.142@gmail.com>
2023-09-29 17:18:00 +1000

    More control signals and opcodes

--- src/GenControl.java
+++ src/GenControl.java
@@ -1,6 +1,5 @@
-import java.io.BufferedOutputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+
 public class GenControl {
 
     /************************************
@@ -92,52 +91,266 @@ public class GenControl {
     private static char[] ctrl2a = new char[1<<12];
     private static char[] ctrl2b = new char[1<<12];
 
-    private static int do_instruction(int opcode, int flags) {
+    private static String[] opcodes = new String[256];
+
+    private static int do_instruction(int opcode, int flags, BufferedWriter bw) throws IOException
+    {
         boolean carry = (flags & 0x01) != 0;
         boolean zero = ((flags >> 1) & 0x01) != 0;
         boolean neg = ((flags >> 2) & 0x01) != 0;
         boolean over = ((flags >> 3) & 0x01) != 0;
 
         int control_word = 0;
+        String mnemonic = null;
+        int bytes = 1;
 
         //NOP
         // DO NOTHING
+        if (opcode == 0x00) {
+            mnemonic = "NOP";
+        }
 
         // CLR A (A=0)
-        if (opcode == 0x01) {
+        else if (opcode == 0x01) {
             control_word |= ALU_ZERO | DA_ALU | DR_A;
+            mnemonic = "CLR_A";
         }
-        // MOV B,A
+        // MOV A,B
         else if (opcode == 0x02) {
-            control_word |= ALU_A | DA_ALU | DR_B;
+            control_word |= ALU_B | DA_ALU | DR_A;
+            mnemonic = "MOV_A,B";
+        }
+        // NEG A
+        else if (opcode == 0x03) {
+            control_word |= ALU_NEGA | DA_ALU | DR_A;
+            mnemonic = "NEG_A";
         }
         // INC A
-        else if (opcode == 0x03) {
+        else if (opcode == 0x04) {
             control_word |= ALU_INCA | DA_ALU | DR_A;
+            mnemonic = "INC_A";
         }
         // DEC A
-        else if (opcode == 0x04) {
+        else if (opcode == 0x05) {
             control_word |= ALU_DECA | DA_ALU | DR_A;
+            mnemonic = "DEC_A";
         }
         // ADD A,B
-        else if (opcode == 0x05) {
+        else if (opcode == 0x06) {
             control_word |= ALU_APLUSB | DA_ALU | DR_A;
+            mnemonic = "ADD_A,B";
         }
         // ADC A,B
-        else if (opcode == 0x06) {
+        else if (opcode == 0x07) {
             if (carry) control_word |= ALU_APLUSBPLUS1 | DA_ALU | DR_A;
             else control_word |= ALU_APLUSB | DA_ALU | DR_A;
+            mnemonic = "ADC_A,B";
         }
         // SUB A,B
-        else if (opcode == 0x07) {
+        else if (opcode == 0x08) {
             control_word |= ALU_AMINUSB | DA_ALU | DR_A;
+            mnemonic = "SUB_A,B";
         }
         // SBB A,B
-        else if (opcode == 0x08) {
+        else if (opcode == 0x09) {
             if (carry) control_word |= ALU_AMINUSBMINUS1 | DA_ALU | DR_A;
-            else control_word |= ALU_AMINUSBMINUS1 | DA_ALU | DR_A;
+            else control_word |= ALU_AMINUSB | DA_ALU | DR_A;
+            mnemonic = "SBB_A,B";
+        }
+        // MLL A,B
+        else if (opcode == 0x0a) {
+            control_word |= ALU_AMULTBLO | DA_ALU | DR_A;
+            mnemonic = "MLL_A,B";
+        }
+        // MLH A,B
+        else if (opcode == 0x0b) {
+            control_word |= ALU_AMULTBHI | DA_ALU | DR_A;
+            mnemonic = "MLH_A,B";
+        }
+        // DIV A,B
+        else if (opcode == 0x0c) {
+            control_word |= ALU_ADIVB | DA_ALU | DR_A;
+            mnemonic = "DIV_A,B";
+        }
+        // MOD A,B
+        else if (opcode == 0x0d) {
+            control_word |= ALU_AMODB | DA_ALU | DR_A;
+            mnemonic = "MOD_A,B";
+        }
+        // AND A,B
+        else if (opcode == 0x0e) {
+            control_word |= ALU_AANDB | DA_ALU | DR_A;
+            mnemonic = "AND_A,B";
+        }
+        // OR A,B
+        else if (opcode == 0x0f) {
+            control_word |= ALU_AORB | DA_ALU | DR_A;
+            mnemonic = "OR_A,B";
+        }
+        // XOR A,B
+        else if (opcode == 0x10) {
+            control_word |= ALU_AXORB | DA_ALU | DR_A;
+            mnemonic = "XOR_A,B";
+        }
+        // NOT A
+        else if (opcode == 0x011) {
+            control_word |= ALU_NOTA | DA_ALU | DR_A;
+            mnemonic = "NOT_A";
+        }
+        // DIV10
+        else if (opcode == 0x12) {
+            control_word |= ALU_ADIV10B | DA_ALU | DR_A;
+            mnemonic = "DIV10_A,B";
+        }
+        // MOD10
+        else if (opcode == 0x13) {
+            control_word |= ALU_AMOD10B | DA_ALU | DR_A;
+            mnemonic = "MOD10_A,B";
         }
 
+
+        // CLR B (B=0)
+        else if (opcode == 0x21) {
+            control_word |= ALU_ZERO | DA_ALU | DR_B;
+            mnemonic = "CLR_B";
+        }
+        // MOV B, A
+        else if (opcode == 0x22) {
+            control_word |= ALU_A | DA_ALU | DR_B;
+            mnemonic = "MOV_B,A";
+        }
+        // NEG B
+        else if (opcode == 0x23) {
+            control_word |= ALU_NEGB | DA_ALU | DR_B;
+            mnemonic = "NEG_B";
+        }
+        // INC B
+        else if (opcode == 0x24) {
+            control_word |= ALU_INCB | DA_ALU | DR_B;
+            mnemonic = "INC_B";
+        }
+        // DEC B
+        else if (opcode == 0x25) {
+            control_word |= ALU_DECB | DA_ALU | DR_B;
+            mnemonic = "DEC_B";
+        }
+        // ADD B,A
+        else if (opcode == 0x26) {
+            control_word |= ALU_APLUSB | DA_ALU | DR_B;
+            mnemonic = "ADD_B,A";
+        }
+        // ADC B,A
+        else if (opcode == 0x27) {
+            if (carry) control_word |= ALU_APLUSBPLUS1 | DA_ALU | DR_B;
+            else control_word |= ALU_APLUSB | DA_ALU | DR_B;
+            mnemonic = "ADC_B,A";
+        }
+        // SUB B,A
+        else if (opcode == 0x28) {
+            control_word |= ALU_BMINUSA | DA_ALU | DR_B;
+            mnemonic = "SUB_B,A";
+        }
+        // SBB B,A
+        else if (opcode == 0x29) {
+            if (carry) control_word |= ALU_BMINUSAMINUS1 | DA_ALU | DR_B;
+            else control_word |= ALU_BMINUSA | DA_ALU | DR_B;
+            mnemonic = "SBB_B,A";
+        }
+        // MLL B,A
+        else if (opcode == 0x2a) {
+            control_word |= ALU_AMULTBLO | DA_ALU | DR_B;
+            mnemonic = "MLL_B,A";
+        }
+        // MLH B,A
+        else if (opcode == 0x2b) {
+            control_word |= ALU_AMULTBHI | DA_ALU | DR_B;
+            mnemonic = "MLH_B,A";
+        }
+        // DVB A,B
+        else if (opcode == 0x2c) {
+            control_word |= ALU_ADIVB | DA_ALU | DR_B;
+            mnemonic = "DVB_A,B";
+        }
+        // MDB A,B
+        else if (opcode == 0x2d) {
+            control_word |= ALU_AMODB | DA_ALU | DR_B;
+            mnemonic = "MDB_A,B";
+        }
+        // AND B,A
+        else if (opcode == 0x2e) {
+            control_word |= ALU_AANDB | DA_ALU | DR_B;
+            mnemonic = "AND_B,A";
+        }
+        // OR B,A
+        else if (opcode == 0x2f) {
+            control_word |= ALU_AORB | DA_ALU | DR_B;
+            mnemonic = "OR_B,A";
+        }
+        // XOR B,A
+        else if (opcode == 0x30) {
+            control_word |= ALU_AXORB | DA_ALU | DR_B;
+            mnemonic = "XOR_B,A";
+        }
+        // NOT B
+        else if (opcode == 0x031) {
+            control_word |= ALU_NOTB | DA_ALU | DR_B;
+            mnemonic = "NOT_B";
+        }
+        // DVB10
+        else if (opcode == 0x32) {
+            control_word |= ALU_ADIV10B | DA_ALU | DR_B;
+            mnemonic = "DVB10_A,B";
+        }
+        // MDB10
+        else if (opcode == 0x33) {
+            control_word |= ALU_AMOD10B | DA_ALU | DR_B;
+            mnemonic = "MDB10_A,B";
+        }
+
+
+        // STO 0 (MEM=0)
+        else if (opcode == 0x41) {
+            control_word |= ALU_ZERO | DA_ALU | DR_MEM;
+            mnemonic = "STO 0";
+        }
+        // STO A
+        else if (opcode == 0x42) {
+            control_word |= ALU_A | DA_ALU | DR_MEM;
+            mnemonic = "STO_A";
+        }
+        // STO B
+        else if (opcode == 0x43) {
+            control_word |= ALU_B | DA_ALU | DR_MEM;
+            mnemonic = "STO_B";
+        }
+        // LDA
+        else if (opcode == 0x44) {
+            control_word |= DA_MEM | DR_A;
+            mnemonic = "LDA";
+        }
+        // LDB
+        else if (opcode == 0x45) {
+            control_word |= DA_MEM | DR_B;
+            mnemonic = "LDB";
+        }
+        // LDA K
+        else if (opcode == 0x46) {
+            control_word |= DA_CONSTANT | DR_A;
+            mnemonic = "LCA";
+            bytes = 2;
+        }
+        // LDB K
+        else if (opcode == 0x47) {
+            control_word |= DA_CONSTANT | DR_B;
+            mnemonic = "LCB";
+            bytes = 2;
+        }
+
+
+
+        if (mnemonic != null && flags == 0x00) {
+            bw.write(String.format("%02x %d %s\n", opcode, bytes, mnemonic));
+        }
         return control_word;
     }
 
@@ -167,17 +380,25 @@ public class GenControl {
 
     public static void main(String[] args) {
         // TODO: Read ROMs first and compare to indicate which ROMs changed
+        BufferedWriter bw;
         System.out.println("Creating Control Signals for Pip pipelined CPU");
-        for (int flags = 0; flags < 2; flags++) {
-            for (int opcode = 0; opcode < 0x100; opcode++) {
-                int control_word = do_instruction(opcode, flags);
-                System.out.printf("%02x %08x   %s\n", opcode, control_word, toBinary(control_word));
-                int addr = opcode | flags << 8;
-                ctrl1b[addr] = (char) ((control_word) & 0xff);
-                ctrl1a[addr] = (char) ((control_word >> 8) & 0xff);
-                ctrl2b[addr] = (char) ((control_word >> 16) & 0xff);
-                ctrl2a[addr] = (char) ((control_word >> 24) & 0xff);
+        try {
+            bw = new BufferedWriter(new FileWriter("opcodes", false));
+            for (int flags = 0; flags < 2; flags++) {
+                for (int opcode = 0; opcode < 0x100; opcode++) {
+                    int control_word = do_instruction(opcode, flags, bw);
+                    System.out.printf("%02x %08x   %s\n", opcode, control_word, toBinary(control_word));
+                    int addr = opcode | flags << 8;
+                    ctrl1b[addr] = (char) ((control_word) & 0xff);
+                    ctrl1a[addr] = (char) ((control_word >> 8) & 0xff);
+                    ctrl2b[addr] = (char) ((control_word >> 16) & 0xff);
+                    ctrl2a[addr] = (char) ((control_word >> 24) & 0xff);
+                }
             }
+            bw.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            System.exit(1);
         }
         // Write ROMS
         write_bytes("ctrl1a.bin", ctrl1a);
